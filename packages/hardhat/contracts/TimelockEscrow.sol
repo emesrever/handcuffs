302cd15ee5d9453666f3a638159873bbe578de6a
Colfax Selby <colfax.selby@gmail.com>
2021-02-10 14:05:27 -0500

    intermediate timelock impl

--- packages/hardhat/contracts/TimelockEscrow.sol
+++ packages/hardhat/contracts/TimelockEscrow.sol
@@ -3,12 +3,51 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/payment/escrow/ConditionalEscrow.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TimelockEscrow is ConditionalEscrow {
-    function withdrawalAllowed(address payee)
+contract TimelockEscrow is Ownable {
+    struct Vault {
+        uint256 amount;
+        // release time
+    }
+
+    mapping(address => Vault[]) private _vaults;
+
+    function getVaultAmount(address owner, uint256 vaultIndex)
         public
         view
-        override
+        returns (uint256)
+    {
+        return _vaults[owner][vaultIndex].amount;
+    }
+
+    function deposit(address payee) public payable virtual onlyOwner {
+        _vaults[payee].push(Vault({amount: msg.value}));
+    }
+
+    function withdraw(address payable payee, uint256 vaultIndex)
+        public
+        virtual
+        onlyOwner
+    {
+        require(
+            withdrawalAllowed(payee, vaultIndex),
+            "does not meet withdraw requirements"
+        );
+
+        require(_vaults[payee].length > vaultIndex, "vault index too high");
+
+        uint256 payment = _vaults[payee][vaultIndex].amount;
+
+        _vaults[payee][vaultIndex].amount = 0;
+
+        payee.sendValue(payment);
+    }
+
+    function withdrawalAllowed(address payee, uint256 vaultIndex)
+        public
+        view
         returns (bool)
     {
         return true;
