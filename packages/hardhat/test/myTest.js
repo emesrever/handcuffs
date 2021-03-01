60856a4fc402df931c312cb5c5f1af0c768ddfe2
Jack Yin <jack@hatchedlabs.com>
2021-03-01 14:25:43 -0500

    initial test commit

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -1,24 +1,72 @@
 const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
+const { accounts, contract } = require('@openzeppelin/test-environment');
+
+const {
+  BN,           // Big Number support
+  constants,    // Common constants, like the zero address and largest integers
+  expectEvent,  // Assertions for emitted events
+  expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
 
 use(solidity);
 
-describe("My Dapp", function () {
+// `describe` is a Mocha function that allows you to organize your tests. It's
+// not actually needed, but having your tests organized makes debugging them
+// easier. All Mocha functions are available in the global scope.
+
+// `describe` receives the name of a section of your test suite, and a callback.
+// The callback must define the tests of that section. This callback can't be
+// an async function.
+describe("Handcuffs", function () {
+  // Mocha has four functions that let you hook into the the test runner's
+  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
+
+  // They're very useful to setup the environment for tests, and to clean it
+  // up after they run.
+
+  // A common pattern is to declare some variables, and assign them in the
+  // `before` and `beforeEach` callbacks.
   let myContract;
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
+  let owner;
+  let guard1;
+  let guard2;
+  let guard3;
 
-      myContract = await YourContract.deploy();
+  beforeEach(async function()) {
+      // Get the ContractFactory and Signers here.
+      [owner, guard1, guard2, guard3] = await ethers.getSigners();
+
+      const Handcuffs = await ethers.getContractFactory("Handcuffs");
+      myContract = await Handcuffs.deploy();
+
+  }
+
+  describe("Handcuffs", function () {
+    it("Should deploy Handcuffs", async function () {
+      const Handcuffs = await ethers.getContractFactory("Handcuffs");
+
+      myContract = await Handcuffs.deploy();
     });
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
+    describe("Create basic wallet with no time lock and no confirmations required", function () {
+      it("Should be able to deposit", async function () {
+          address payable beneficiary,
+          uint256 numConfirmations,
+          uint256 lock_seconds,
+          address guardianOne,
+          address guardianTwo,
+          address guardianThree
 
-        await myContract.setPurpose(newPurpose);
+        await myContract.createWallet(owner,
+            0, // num confirmations
+            0, // lock_seconds
+            guard1,
+            guard2,
+            guard3
+            );
         expect(await myContract.purpose()).to.equal(newPurpose);
       });
     });
